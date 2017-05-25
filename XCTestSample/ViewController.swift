a6f5b00802314378e0ae4f45cea719a9648f775a
伊東直人 <itounaoto@itounaotonomacbook-pro.local>
2017-05-25 12:07:05 +0900

    Initial Commit

---
+++ XCTestSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCTestSample
+//
+//  Created by 伊東直人 on 2017/05/25.
+//  Copyright © 2017 伊東直人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
